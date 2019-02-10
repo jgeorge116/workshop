017e6ee2df402986a79553b42ee0f91312e447d2
Joel George <joelgeorge@JoelComp.local>
2019-02-09 19:17:39 -0500

    added src file

---
+++ Skeleton_Code.java
@@ -0,0 +1,16 @@
+public class Skeleton_Code {
+    public static void main (String[] args){
+        String myname = "joel";
+        print(myname);
+
+    }
+
+    /**
+    * a function that prints your name
+     **/
+
+    static void print(String name){
+        System.out.println("My name is " + name);
+    }
+
+}
