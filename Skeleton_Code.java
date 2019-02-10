450462927b10938ef4e64f06695fbea514437f91
Joel George <joelgeorge@JoelComp.local>
2019-02-09 19:31:54 -0500

    added another funciton

--- Skeleton_Code.java
+++ Skeleton_Code.java
@@ -1,16 +1,30 @@
 public class Skeleton_Code {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         String myname = "joel";
-        print(myname);
+        PrintName(myname);
+        System.out.println();
+        MakeSomeVariables();
 
     }
 
     /**
-    * a function that prints your name
+     * a function that prints your name
      **/
 
-    static void print(String name){
+    static void PrintName(String name) {
         System.out.println("My name is " + name);
+    }
+
+    static void  MakeSomeVariables(){
+        String name = "mike";
+        char letter = 'f';
+        int num = 10;
+        boolean flag = true;
+        System.out.println("Made variable " + name + " of type String" +
+                        "\nMade variable " + letter + " of type char" +
+                        "\nMade variable " + num + " of type integer" +
+                        "\nMade variable " + flag + " of type boolean");
+
     }
 
 }
