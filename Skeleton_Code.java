100545068907119b21f5a2fa74332362407f3458
Joel George <joelgeorge@JoelComp.local>
2019-02-10 15:28:46 -0500

    added more documentation

--- Skeleton_Code.java
+++ Skeleton_Code.java
@@ -18,15 +18,14 @@ public class Skeleton_Code {
         String full = first + " " + last; //this puts together your first and last name right now, with a space in between
         PrintName(full);
 
-
-
-
         System.out.println();
         ShowIFElseifELseSatements();
         System.out.println();
         ShowExamplesWithTypicalLoops();
         System.out.println();
         EnhancedForLoopExampleWithArray();
+        System.out.println();
+        ShowMathOperators(8,2); //passing in 2 arguments into our function
         System.out.println();
         ShowUseOfUSerInput();
     }
@@ -48,7 +47,6 @@ public class Skeleton_Code {
      **/
 
     static void PrintSomeVariables(String name, char letter, int num, boolean flag) {
-
         System.out.println("Made variable " + name + " of type String" +
                 "\nMade variable " + letter + " of type char" +
                 "\nMade variable " + num + " of type integer" +
@@ -60,9 +58,17 @@ public class Skeleton_Code {
      * a function that shows how conditional statements work
      **/
 
+    static void ShowMathOperators(int a, int b){
+        System.out.println("'"+ a + "'" + "+" + "'" + b + "'" + "is " + (a+b));
+        System.out.println("'"+ a + "'" + "-" + "'" + b + "'" + "is " + (a-b));
+        System.out.println("'"+ a + "'" + "/" + "'" + b + "'" + "is " + (a/b));
+        System.out.println("'"+ a + "'" + "*" + "'" + b + "'" + "is " + (a*b));
+        System.out.println("'"+ a + "'" + "%" + "'" + b + "'" + "is " + (a%b)); //modulo operator (num1/num2) then takes remainder
+    }
+
     static void ShowIFElseifELseSatements() {
         int num2 = 10;
-        if (num2 > 30) {
+        if(num2 > 30) {
             System.out.println("the number is bigger than 30");
         } else if (num2 > 20) {
             System.out.println("the number is bigger than 20");
@@ -71,11 +77,11 @@ public class Skeleton_Code {
         }
 
         String randomString = "hello";
-        if (randomString.equals("bye") || randomString.equals("hello")) {
+        if(randomString.equals("bye") || randomString.equals("hello")) { //1 statement has to be true for the whole if statment to be true
             System.out.println("the if statement is true");
-        } else if (randomString.equals("no") && num2 < 2) {
+        } else if (randomString.equals("no") && num2 < 2) { //both statements must be true for the whole if statement to be true
             System.out.println("the first else if statement is true");
-        } else if (!(num2 == 2)) {
+        } else if (!(num2 == 2)) { //negates the statement if num2 == 2
             System.out.println("the second else if statement is true");
         } else {
             System.out.println("no statements are true");
@@ -89,13 +95,14 @@ public class Skeleton_Code {
     static void ShowExamplesWithTypicalLoops() {
         int num3 = 10;
         System.out.println("while loops example");
-        while (num3 > 0) {
+        while (num3 > 0) {  //this loop will happen 10 times
             System.out.println(num3);
-            num3--;
+            num3--; //decrements the variable by 1
         }
         System.out.println();
         System.out.println("for loop example");
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) { //this loop will happen 10 times
+            //i=0 is the starting condition, i<10 is the stopping condition, i++ increments i by 1
             System.out.println(i);
         }
     }
@@ -105,14 +112,14 @@ public class Skeleton_Code {
      **/
 
     static void EnhancedForLoopExampleWithArray() {
-        int[] intArray = new int[10];
+        int[] intArray = new int[10];  //make an array of length 10
         for (int i = 0; i < intArray.length; i++) {
-            intArray[i] = i;
+            intArray[i] = i; //fills the ith position of the array with i
         }
         System.out.println();
         System.out.println("enhanced for loop example");
-        for (int vars : intArray) {
-            System.out.println(vars);
+        for (int vars : intArray) { //go through every number in the array starting at the beginning
+            System.out.println(vars); //prints the number
         }
     }
 
@@ -121,9 +128,9 @@ public class Skeleton_Code {
      **/
 
     static void ShowUseOfUSerInput() {
-        Scanner myScanner = new Scanner(System.in);
+        Scanner myScanner = new Scanner(System.in); //creates an object that allows one to read user input
         System.out.println("whats your name? ");
-        String usersName = myScanner.nextLine();
+        String usersName = myScanner.nextLine(); //stores the user's input into a string variable
         System.out.println("your name is " + usersName);
     }
 }
